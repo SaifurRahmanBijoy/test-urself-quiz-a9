df4af7504a1f90970f6bb600953ef82f1a859592
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-10-11 21:02:28 +0600

    finished readme for github

--- src/Components/Option/Option.js
+++ src/Components/Option/Option.js
@@ -4,10 +4,10 @@ const Option = ({ option }) => {
   //   console.log(option);
   const { key } = option;
   return (
-    <div className="border-2 border-slate-300 p-2 text-sm rounded-md bg-slate-100">
+    <div className="border-2 border-slate-300 p-2 text-sm rounded-md bg-slate-100 hover:bg-slate-300">
       <input type="radio" id={key} />
       <label className="ml-3" for="child">
-        {option}r
+        {option}
       </label>
     </div>
   );
